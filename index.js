ca4dc073c6df20d209692132ebc711d93f96c4ed
My N <myEmail@example.com>
2019-09-25 09:38:01 +0800

    Updated

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var port1 = process.env.PORT||3000; 
 
 connectedUsers = [];
 app.use(express.static('public'));
-server.listen(port1);1
+server.listen(port1);
 console.log('Server started.');
 
 io.sockets.on('connection' , clientConnected);
